ba3f308fa54e4a212edfac5f924c5c21b1ec849a
Marte De Leeuw <martedeleeuw@hotmail.com>
2021-03-23 16:25:39 +0100

    added basic version of customer -model and -loader

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
+declare(strict_types=1);
+
+require 'models/Customer.php';
+require 'models/DbConnect.php';
+require 'loaders/CustomerLoader.php';
+
 include("view/includes/header.php");
+
 ?>
+
 <h1>Test</h1>
 
 <?php
