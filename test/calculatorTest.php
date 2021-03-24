0dc58035239ec09e0b443f3aa82608f91a51c3e5
KaiHelsen <kai@Helsen.info>
2021-03-24 14:10:11 +0100

    changed composer setup and re-made test file

--- test/calculatorTest.php
+++ test/calculatorTest.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
-require("./vendor/autoload.php");
+
+namespace test;
+require_once ("./vendor/autoload.php");
+
+//use ComposerIncludeFiles\models\Calculator;
 use PHPUnit\Framework\TestCase;
 
 //use cmd command ./vendor/bin/phpunit test
@@ -11,4 +15,28 @@ final class calculatorTest extends TestCase
     {
         self::assertTrue(true);
     }
+
+    public function provideTestData(): array
+    {
+        return [
+            [6, 10, 4, 'FIXED', 'expect 6'],
+            [0, 10, 12, 'FIXED', 'expect 0']
+        ];
+    }
+
+    /**
+     * @dataProvider  provideTestData
+     * @param float $expectedResult
+     * @param int $price
+     * @param int $discount
+     * @param string $type
+     * @param string $expectMsg
+     */
+    public function testBaseFunctionality(float $expectedResult, int $price, int $discount, string $type, string $expectMsg): void
+    {
+        $calculator = new Calculator;
+        self::assertequals($expectedResult, $calculator->calculate($price, $discount, $type), $expectMsg);
+    }
+
+
 }
