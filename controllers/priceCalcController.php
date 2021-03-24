043c611624413b2f5c55a47bda0120d612654460
KaiHelsen <kai@Helsen.info>
2021-03-24 08:55:50 +0100

    test

---
+++ controllers/priceCalcController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class calcController extends controller
+{
+
+    /**
+     * @inheritDoc
+     */
+    function render(array $GET, array $POST): void
+    {
+        // TODO: Implement render() method.
+    }
+}
