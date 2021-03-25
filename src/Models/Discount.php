ceae1f56354f8c05f0a7cf6d40acb356af1a6032
Marte De Leeuw <martedeleeuw@hotmail.com>
2021-03-25 10:37:58 +0100

    forgot to add Discount and Loader

---
+++ src/Models/Discount.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class Discount
+{
+    public const VARIABLE = 'VARIABLE';
+    public const FIXED = 'FIXED';
+
+    private string $type;
+    private int $amount;
+
+    /**
+     * Discount constructor.
+     * @param string $type
+     * @param int $amount
+     */
+    public function __construct(string $type, int $amount)
+    {
+        $this->type = $type;
+        $this->amount = $amount;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+
+}
