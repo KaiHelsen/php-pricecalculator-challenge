60add3bd864b19a3e6460aa2e193167c6ccbd89b
Marte De Leeuw <martedeleeuw@hotmail.com>
2021-03-26 09:04:56 +0100

    connection update

--- src/Models/DbConnect.php
+++ src/Models/DbConnect.php
@@ -3,15 +3,10 @@
 
 class DbConnect
 {
-    private const HOST = "localhost";
-    private string $user = "becode";
-    private string $pwd = "becode123";
-    private string $dbName = "pricecalculator";
-
     public function connect() : PDO
     {
-        $dsn = 'mysql:host=' . self::HOST . ';dbname=' . $this->dbName;
-        $pdo = new PDO($dsn, $this->user, $this->pwd);
+        $dsn = 'mysql:host=' . HOST . ';dbname=' . DBNAME;
+        $pdo = new PDO($dsn, USER, PWD);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         return $pdo;
     }
